4788ec80f2761acacf62a45ea1757cf1e85f342e
kojiado <kojiado@outlook.com>
2023-04-12 17:00:08 +0200

    Gitignore Update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "17b26e510963ede7"; }
+/******/ 		__webpack_require__.h = function() { return "f3f2ee0282714f3b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
