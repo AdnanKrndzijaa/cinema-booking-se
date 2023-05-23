ae8e3a8c1e609c834d42632cbf6d0acde9d99ae8
kojiado <kojiado@outlook.com>
2023-05-23 15:17:50 +0200

    Error Fixing

--- components/Actor.tsx
+++ components/Actor.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import Image from 'next/image';
 
 interface ActorProps {
     avatar: string;
     realName: string;
     movieName: string;
-    key: String;
+    key: number;
 }
 
 const Actor: React.FC<ActorProps> = ({avatar, realName, movieName,key}) => {
   return (
     <div key={key} className='flex items-center'>
-        <img src={avatar} className='w-[62px] h-[62px] object-cover rounded-full'></img>
+        <Image alt="Actor Image" src={avatar} className='w-[62px] h-[62px] object-cover rounded-full'/>
         <div className='flex flex-col ml-[16px]'>
             <p className='text-white font-bold mb-[4px] whitespace-nowrap overflow-hidden'>{realName}</p>
             <p className='text-text whitespace-nowrap'>{movieName}</p>
