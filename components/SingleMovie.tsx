a8e36dcc862def040d2464caa878948e2236e6ed
kojiado <kojiado@outlook.com>
2023-05-23 14:21:32 +0200

    Single Movie Key Fix

--- components/SingleMovie.tsx
+++ components/SingleMovie.tsx
@@ -10,7 +10,7 @@ const SingleMovie: React.FC<SingleMovieProps> = ({id,bannerUrl}) => {
     const router = useRouter();
 
   return (
-    <div className='single-movie'>
+    <div key={id} className='single-movie'>
         <img onClick={() => router.push(`/movie/${id}`)} className='h-full rounded-[10px] hover:-[2px] hover:outline-white' src={bannerUrl}></img>
     </div>
   )
