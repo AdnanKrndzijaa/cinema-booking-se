128b01630c03295cc81c1a2846b8a9fccf0ec167
Adnan Krndzija <92021913+AdnanKrndzijaa@users.noreply.github.com>
2023-05-31 19:26:46 +0200

    get all users, get user by id added

---
+++ pages/api/users/index.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prismadb from '@/lib/prismadb';
+import serverAuth from '@/lib/serverAuth';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    if (req.method !== 'GET') {
+      return res.status(405).end();
+    }
+
+    const { currentUser } = await serverAuth(req, res);
+    
+    if (!currentUser ) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const users = await prismadb.user.findMany({
+      where: {
+        type: 'User'
+      }
+    });
+
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).json({ error: `Something went wrong: ${error}` });
+  }
+}
