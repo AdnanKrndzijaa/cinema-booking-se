d6f09a6d135f44eba71b6c63367a473fe5ef2a80
Adnan Krndzija <92021913+AdnanKrndzijaa@users.noreply.github.com>
2023-06-18 17:00:56 +0200

    add comments to movies section

--- pages/api/movies/[movieId].ts
+++ pages/api/movies/[movieId].ts
@@ -1,32 +1,32 @@
-import {NextApiRequest, NextApiResponse} from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 import prismadb from '@/lib/prismadb';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if(req.method !== 'GET') {
-        return res.status(405).end();
+  if (req.method !== 'GET') {
+    return res.status(405).end(); // Method Not Allowed
+  }
+
+  try {
+    const { movieId } = req.query;
+
+    if (typeof movieId !== 'string') {
+      throw new Error('Invalid Id');
     }
 
-    try {
-        const { movieId } = req.query;
-
-        if (typeof movieId !== 'string') {
-        throw new Error('Invalid Id');
-        }
-
-        if (!movieId) {
-        throw new Error('Missing Id');
-        }
-
-        const movies = await prismadb.movie.findUnique({
-        where: {
-            id: movieId
-        }
-        });
-
-        return res.status(200).json(movies)
-
-    } catch(error) {
-        console.log({ error })
-        return res.status(500).end();
+    if (!movieId) {
+      throw new Error('Missing Id');
     }
+
+    const movies = await prismadb.movie.findUnique({
+      where: {
+        id: movieId
+      }
+    });
+
+    return res.status(200).json(movies); // Return the movie as JSON response
+
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).end(); // Internal Server Error
+  }
 }
--- pages/api/movies/index.ts
+++ pages/api/movies/index.ts
@@ -1,17 +1,23 @@
-import {NextApiRequest, NextApiResponse} from 'next';
+// Import the necessary modules
+import { NextApiRequest, NextApiResponse } from 'next';
 import prismadb from '@/lib/prismadb';
 import serverAuth from '@/lib/serverAuth';
 
+// Define the handler function for the API endpoint
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if(req.method !== 'GET') {
-        return res.status(405).end();
-    }
+  // Check the request method
+  if (req.method !== 'GET') {
+    return res.status(405).end();
+  }
 
-    try {
-        const movies = await prismadb.movie.findMany();
-        return res.status(200).json(movies);
-    } catch(error) {
-        console.log({ error })
-        return res.status(500).end();
-    }
+  try {
+    // Retrieve the list of movies from the database
+    const movies = await prismadb.movie.findMany();
+
+    // Return the movies as the response
+    return res.status(200).json(movies);
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).end();
+  }
 }
