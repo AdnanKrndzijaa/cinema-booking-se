84430e0eb9c03c930ccfdfb10834fe9a110a7968
Adnan Krndzija <92021913+AdnanKrndzijaa@users.noreply.github.com>
2023-06-18 12:06:00 +0200

    deleteShowtime fixed

--- pages/api/showtimes/deleteShowtime.ts
+++ pages/api/showtimes/deleteShowtime.ts
@@ -1,47 +1,68 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import prismadb from '@/lib/prismadb';
 
-class ShowtimeBuilder {
-  private showtimeId: string;
+class Database {
+  private static instance: Database | null = null;
 
-  public withShowtimeId(showtimeId: string): ShowtimeBuilder {
-    this.showtimeId = showtimeId;
-    return this;
+  private constructor() {
+    // Private constructor to prevent direct instantiation
   }
 
-  public async deleteShowtime(): Promise<void> {
-    const existingShowtime = await prismadb.showtime.findUnique({
-      where: {
-        id: this.showtimeId
-      }
-    });
-
-    if (!existingShowtime) {
-      throw new Error('Showtime not found');
+  public static getInstance(): Database {
+    if (!Database.instance) {
+      Database.instance = new Database();
     }
 
-    await prismadb.showtime.delete({
-      where: {
-        id: this.showtimeId
+    return Database.instance;
+  }
+
+  public async deleteShowtime(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+    try {
+      if (req.method !== 'DELETE') {
+        res.status(405).end();
+        return;
       }
-    });
+
+      const { showtimeId } = req.body;
+      console.log('Request Body:', req.body);
+
+      if (!showtimeId) {
+        res.status(400).json({ error: 'Showtime ID is required' });
+        return;
+      }
+
+      // Check if the showtime exists
+      const existingShowtime = await prismadb.showtime.findUnique({
+        where: {
+          id: showtimeId,
+        },
+      });
+
+      if (!existingShowtime) {
+        res.status(404).json({ error: 'Showtime not found' });
+        return;
+      }
+
+      // Delete the showtime
+      await prismadb.showtime.delete({
+        where: {
+          id: showtimeId,
+        },
+      });
+
+      res.status(200).json({ message: 'Showtime deleted successfully' });
+    } catch (error) {
+      res.status(400).json({ error: `Something went wrong: ${error}` });
+    }
   }
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    if (req.method !== 'DELETE') {
-      return res.status(405).end();
-    }
+  const dbInstance = Database.getInstance();
 
-    const { showtimeId } = req.body;
-
-    await new ShowtimeBuilder()
-      .withShowtimeId(showtimeId)
-      .deleteShowtime();
-
-    return res.status(200).json({ message: 'Showtime deleted successfully' });
-  } catch (error) {
-    return res.status(500).json({ error: `Something went wrong: ${error}` });
+  if (req.method === 'DELETE') {
+    await dbInstance.deleteShowtime(req, res);
+  } else {
+    res.status(405).end(); // Method Not Allowed
   }
 }
