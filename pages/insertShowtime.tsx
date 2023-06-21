b87b4a05f839528006311265a5f92a7434436be8
Adnan Krndzija <92021913+AdnanKrndzijaa@users.noreply.github.com>
2023-06-21 22:11:43 +0200

    small changes

--- pages/insertShowtime.tsx
+++ pages/insertShowtime.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import axios from 'axios';
 import 'react-calendar/dist/Calendar.css';
@@ -8,20 +8,35 @@ import Button from '@/components/Button';
 import Link from 'next/link';
 
 const add_showtime = () => {
-	const [selectMovie, setSelectMovie] = useState(false);
+	const [movies, setMovies] = useState([]);
+	const [selectedMovie, setSelectedMovie] = useState(null);
 	const [dateTime, setDateTime] = useState('');
 	const [type, setType] = useState('');
 	
 	const [value, onChange] = useState(new Date());
 
+	useEffect(() => {
+		// Fetch movies from the backend API
+		const fetchMovies = async () => {
+		  try {
+			const response = await axios.get('/api/movies');
+			setMovies(response.data);
+		  } catch (error) {
+			console.error('Error fetching movies:', error);
+		  }
+		};
+	
+		fetchMovies();
+	  }, []);
+
 	const handleSelect = () => {
-		setSelectMovie(!selectMovie);
+		setSelectedMovie(!selectedMovie);
 	};
 
 	const addShowtime = useCallback(async () => {
 		try {
 			await axios.post('/api/showtimes/insertShowtime', {
-				selectMovie,
+				selectedMovie,
 				dateTime,
 				type
 			});
@@ -31,7 +46,7 @@ const add_showtime = () => {
 		} catch(error) {
 			console.log(error);
 		}
-	}, [selectMovie,
+	}, [selectedMovie,
 		dateTime,
 	  type]); 
 
@@ -42,31 +57,56 @@ const add_showtime = () => {
             <div className='w-2/6 border-primaryvariant1 border-[1px] rounded-[10px] p-[28px] h-fit'>
                 <h5 className='text-white text-[20px] mb-[16px]'>Movie</h5>
                 <p className='text-text mb-[28px]'>Choose the movie for which you want to create showtimes.</p>
-                <div onClick={handleSelect} className='w-[350px] cursor-pointer rounded-[10px] bg-primaryvariant1 p-[16px] flex justify-between items-center'>
-                    <p className='text-white pointer-events-none'>Select Option</p>
-                    <svg className={`${selectMovie ? 'rotate-180' : 'rotate-0'} pointer-events-none`} xmlns="http://www.w3.org/2000/svg" width="10" height="7" viewBox="0 0 10 7">
-                        <path id="Polygon_3" data-name="Polygon 3" d="M4.186,1.139a1,1,0,0,1,1.627,0l3.057,4.28A1,1,0,0,1,8.057,7H1.943a1,1,0,0,1-.814-1.581Z" transform="translate(10 7) rotate(180)" fill="#8b898c"/>
-                    </svg>
-                </div>
-                <div className={`${selectMovie ? 'block' : 'hidden'} w-[350px] select-tab rounded-[10px] bg-primaryvariant1 mt-[16px]`}>
-                    <input className='bg-transparent w-full p-[16px] focus:outline-none text-white text-[16px] font-opensans' type="text" name="Search" id="Search" placeholder="Search"></input>
-                    <div className='movie p-[16px] flex border-t-[1px] movie-option'>
-                        <img className='w-[22px] h-[22px] rounded-[5px] object-cover' src="https://upload.wikimedia.org/wikipedia/en/0/0b/FBAWTFT-poster.jpg"></img>
-                        <p className='text-white ml-[20px] truncate'>Fantastic beasts and where to find them</p>
-                    </div>
-                    <div className='movie p-[16px] flex border-t-[1px] movie-option'>
-                        <img className='w-[22px] h-[22px] rounded-[5px] object-cover' src="https://upload.wikimedia.org/wikipedia/en/5/52/Dune_Part_Two_poster.jpeg"></img>
-                        <p className='text-white ml-[20px] truncate'>Dune</p>
-                    </div>
-                    <div className='movie p-[16px] flex border-t-[1px] movie-option'>
-                        <img className='w-[22px] h-[22px] rounded-[5px] object-cover' src="https://m.media-amazon.com/images/M/MV5BNzAxYmQzYmYtYjEzYS00ZTFkLWE3NjUtODhjOTQzM2U5ZWI1XkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg"></img>
-                        <p className='text-white ml-[20px] truncate'>The Platform</p>
-                    </div>
-                </div>
-                <div className='mt-[28px] w-[160px] h-[246px] flex items-center justify-center rounded-[10px] bg-primaryvariant1 border-[1px] border-dashed border-primaryvariant2'>
-                    <p className='text-text'>Banner<br></br>Preview</p>
-                </div>
-								<h4 className='text-[20px] text-white mt-[28px]'>Movie Name</h4>
+                <div
+        onClick={handleSelect}
+        className="w-[350px] cursor-pointer rounded-[10px] bg-primaryvariant1 p-[16px] flex justify-between items-center"
+      >
+        <p className="text-white pointer-events-none">Select Option</p>
+        <svg
+          className={`${selectedMovie ? 'rotate-180' : 'rotate-0'} pointer-events-none`}
+          xmlns="http://www.w3.org/2000/svg"
+          width="10"
+          height="7"
+          viewBox="0 0 10 7"
+        >
+          <path
+            id="Polygon_3"
+            data-name="Polygon 3"
+            d="M4.186,1.139a1,1,0,0,1,1.627,0l3.057,4.28A1,1,0,0,1,8.057,7H1.943a1,1,0,0,1-.814-1.581Z"
+            transform="translate(10 7) rotate(180)"
+            fill="#8b898c"
+          />
+        </svg>
+      </div>
+      <div className={`${selectedMovie ? 'block' : 'hidden'} w-[350px] select-tab rounded-[10px] bg-primaryvariant1 mt-[16px]`}>
+	  {movies.map((movie) => (
+		<div
+			key={movie.id}
+			className="movie p-[16px] flex border-t-[1px] movie-option"
+			onClick={() => handleSelect()}
+		>
+			<img
+			className="w-[22px] h-[22px] rounded-[5px] object-cover"
+			src={movie.bannerUrl}
+			alt={movie.title}
+			/>
+			<p className="text-white ml-[20px] truncate">{movie.title}</p>
+		</div>
+		))}
+      </div>
+      <div className="mt-[28px] w-[160px] h-[246px] flex items-center justify-center rounded-[10px] bg-primaryvariant1 border-[1px] border-dashed border-primaryvariant2">
+		{selectedMovie ? (
+			<img
+			className="w-full h-full rounded-[10px] object-cover"
+			src={selectedMovie.bannerUrl}
+			alt={selectedMovie.title}
+			/>
+		) : (
+			<p className="text-text">Banner<br />Preview</p>
+		)}
+		</div>
+
+			<h4 className='text-[20px] text-white mt-[28px]'>Movie Name</h4>
             </div>
 						<div className='w-4/6 flex flex-col gap-[28px]'>
 							<div className='border-primaryvariant1 border-[1px] rounded-[10px] p-[28px]'>
